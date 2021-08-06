e837bb0fab44680958eb14ba145efc0f6276180f
Johannes Jones <j.fl.jones@gmail.com>
2021-08-06 18:38:52 +0200

    borrow & payOff

--- src/contracts/dBank.sol
+++ src/contracts/dBank.sol
@@ -11,11 +11,15 @@ contract dBank {
   //add mappings
   mapping(address => uint) public etherBalanceOf;
   mapping(address => uint) public depositStart;
+  mapping(address => uint) public collateralEther;
   mapping(address => bool) public isDeposited;
+  mapping(address => bool) public isBorrowed;
 
   //add events
   event Deposit(address indexed user, uint etherAmount, uint timeStart);
   event Withdraw(address indexed user, uint etherBalance, uint depositTime, uint interest);
+  event Borrow(address indexed user, uint collateralEtherAmount, uint borrowedTokenAmount);
+  event PayOff(address indexed user, uint fee);
 
   //pass as constructor argument deployed Token contract
   constructor(Token _token) public {
@@ -72,28 +76,37 @@ contract dBank {
   function borrow() payable public {
     //check if collateral is >= than 0.01 ETH
     //check if user doesn't have active loan
+    require(msg.value >= 1e16, 'Collateral must be >= 0.01 ETH');
+    require(isBorrowed[msg.sender] == false, 'Loan already taken');
 
     //add msg.value to ether collateral
-
+    collateralEther[msg.sender] = collateralEther[msg.sender] + msg.value;
     //calc tokens amount to mint, 50% of msg.value
-
+    uint tokensToMint = collateralEther[msg.sender] / 2;
     //mint&send tokens to user
-
+    token.mint(msg.sender, tokensToMint);
     //activate borrower's loan status
-
+    isBorrowed[msg.sender] = true;
+    
     //emit event
+    emit Borrow(msg.sender, collateralEther[msg.sender], tokensToMint);
   }
 
   function payOff() public {
     //check if loan is active
+    require(isBorrowed[msg.sender] == true, 'Error, loan not active');
     //transfer tokens from user back to the contract
-
+    require(token.transferFrom(msg.sender, address(this), collateralEther[msg.sender]/2), "Error, can't receive tokens"); //must approve dBank 1st
+    
     //calc fee
-
+    uint fee = collateralEther[msg.sender]/10; //calc 10% fee
     //send user's collateral minus fee
-
+    msg.sender.transfer(collateralEther[msg.sender]-fee);
     //reset borrower's data
-
+    collateralEther[msg.sender] = 0;
+    isBorrowed[msg.sender] = false;
+    
     //emit event
+    emit PayOff(msg.sender, fee);
   }
 }
